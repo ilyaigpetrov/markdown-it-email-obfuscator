469ca708d6dbffc0f6ecd4ce91ef3a667403fcff
ilyaigpetrov <ilyaigpetrov@gmail.com>
2018-06-11 20:59:00 +0500

    Remove linkify option from the example

--- ._example.js
+++ ._example.js
@@ -2,9 +2,8 @@
 
 const mdEmailObfuscator = require('.');
 
-const md = require('markdown-it')({
-  linkify: true,
-}).use(mdEmailObfuscator);
+const md = require('markdown-it')()
+  .use(mdEmailObfuscator);
 
 const input = 'AA<homer.j.simpson@example.com>BB';
 console.log(md.render(input));
